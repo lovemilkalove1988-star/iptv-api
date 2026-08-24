b5daf31cc1652a0afd25d28ba1893c438c56a75b
lovemilkalove1988-star <love.milka.love.1988@gmail.com>
2026-08-25 01:05:31 +0500

    Update import.js

--- import.js
+++ import.js
@@ -4,28 +4,35 @@ const db = require("./database");
 const file = "ru.kz.m3u";
 
 async function importM3U() {
-  const data = fs.readFileSync(file, "utf8");
 
+  const data = fs.readFileSync(file, "utf8");
   const lines = data.split("\n");
 
   let name = "";
   let url = "";
   let category = "";
+  let logo = "";
 
   for (const line of lines) {
+
     if (line.startsWith("#EXTINF")) {
+
       name = line.split(",").pop().trim();
 
       const group = line.match(/group-title="([^"]+)"/);
       category = group ? group[1] : "Other";
+
+      const logoMatch = line.match(/tvg-logo="([^"]+)"/);
+      logo = logoMatch ? logoMatch[1] : "";
     }
 
     if (line.startsWith("http")) {
+
       url = line.trim();
 
       await db.query(
-        "INSERT INTO channels (name, url, category) VALUES ($1,$2,$3)",
-        [name, url, category]
+        "INSERT INTO channels (name, url, category, logo) VALUES ($1,$2,$3,$4)",
+        [name, url, category, logo]
       );
 
       console.log("Added:", name);
