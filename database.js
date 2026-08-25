5dcdab8df598eee04a9b608758584a5c34f36a8d
Rinat <love.milka.love.1988@gmail,com>
2026-08-25 23:32:22 +0500

    Working IPTV server with PostgreSQL

--- database.js
+++ database.js
@@ -1,15 +1,11 @@
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
-console.log("PGUSER:", process.env.PGUSER);
-console.log("PGHOST:", process.env.PGHOST);
-console.log("PGDATABASE:", process.env.PGDATABASE);
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "iptv",
+  password: "admin",
+  port: 5432
 });
 
 module.exports = pool;
