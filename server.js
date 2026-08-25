492327987a8c341e55d2cf4d02494d2431499d08
lovemilkalove1988-star <love.milka.love.1988@gmail.com>
2026-08-25 15:59:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/public/admin/index.html");
+});
+
 app.get("/", (req, res) => {
   res.json({
     status: "online",
