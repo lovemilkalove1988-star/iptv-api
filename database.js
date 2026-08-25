03f5bf1c0d81ff0dd8f839b014d28a6188d9b4dd
Rinat <love.milka.love.1988@gmail,com>
2026-08-25 23:44:02 +0500

    Move PostgreSQL credentials to environment

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "iptv",
-  password: "admin",
-  port: 5432
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT)
 });
 
 module.exports = pool;
