71846816d6db69a2549f45e7a410fda00163f669
Rinat <_emaillove.milka.love.1988@gmail.com>
2026-08-25 18:18:39 +0500

    debug database env

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool(
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+console.log("PGUSER:", process.env.PGUSER);
+console.log("PGHOST:", process.env.PGHOST);
+console.log("PGDATABASE:", process.env.PGDATABASE);
   process.env.DATABASE_URL
     ? {
         connectionString: process.env.DATABASE_URL,
